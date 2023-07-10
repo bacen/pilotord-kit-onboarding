c386dda0cecc4ac61ef38080906ff6f2a9f8f1db
Milton Viegas <86444588+Milton-Viegas@users.noreply.github.com>
2023-07-10 14:04:01 +0000

    add RealTokenizado.sol

---
+++ contracts/RealTokenizado.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts
+
+pragma solidity >=0.8.2;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import { console } from "hardhat/console.sol";
