735e74c8b00e362ccef15e9083c90c515045ceaf
Diego De Lucca <138592528+dbzanette@users.noreply.github.com>
2023-10-16 17:00:52 -0300

    Update example5.ts

--- exemplos/example5.ts
+++ exemplos/example5.ts
@@ -43,7 +43,7 @@ async function tpftOperation1002() {
     abiITPFtOperation1002, 
     tpftOperation1002Address
     ); 
-
+  
   /**
    * Sender refere-se ao cedente - Bacen atua a nome da Secretaria Nacional do Tesouro (STN).
    * Receiver refere-se ao cessionário - Instituição Financeira Participante cadastrada no Real Digital.
@@ -59,7 +59,6 @@ async function tpftOperation1002() {
     operationId: '<Número de operação + data vigente no formato yyyyMMdd>',
     cnpj8Sender: '<CNPJ8 do cedente da operação. Sempre será o CNPJ8 da STN>',
     cnpj8Receiver: '<CNPJ8 do cessionário da operação>',
-    callerPart: '<Parte que está transmitindo o comando da operação>',
     tpftData: {
       acronym: '<A sigla do TPFt>',
       code: "<O código único do TPFt>",
@@ -113,5 +112,3 @@ async function tpftOperation1002() {
      */
     console.log(receiverTransaction.hash);  
 }
-
-
