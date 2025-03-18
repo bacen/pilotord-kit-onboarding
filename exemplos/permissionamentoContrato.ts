34bf4b14c5cbd56675b44b042efd0077f36f6929
Marcus Vinicius Cursino Suares <marcus.suares@bcb.gov.br>
2025-03-18 17:20:48 -0300

    ABI do AccountRules e fix do exemplo

--- exemplos/permissionamentoContrato.ts
+++ exemplos/permissionamentoContrato.ts
@@ -1,11 +1,12 @@
 import { ethers } from "hardhat";
+import abiAccountRules from '../abi/AccountRules.json';
 
 
 //exemplo de código para o participante realizar a permissão de um contrato para ser executado na rede
 async function permissionamentoContrato() {
 
     //0x51FD57d1f7c9539986333C50978D2E9926300e27 - prod nova versão 10/03/25
-    const rulesInstance = await ethers.getContractAt("AccountRules", "0x51FD57d1f7c9539986333C50978D2E9926300e27");
+    const rulesInstance = await ethers.getContractAt(abiAccountRules, "0x51FD57d1f7c9539986333C50978D2E9926300e27");
 
 
     const listedContracts:Array<String> = ["endereço do contrato a ser permissionado"];
